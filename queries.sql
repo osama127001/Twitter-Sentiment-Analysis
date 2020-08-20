4851304e462148bbae53035af64883993d79730d
osama <o4osamakhan@gmail.com>
2020-08-20 19:20:12 +0500

    adding query file

---
+++ queries.sql
@@ -0,0 +1,37 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS raw_tweets
+(
+	created_at string,
+	id string,
+	id_str string,
+	text string,
+	source string,
+	truncated string,
+	user_tw struct
+	<
+		id:string,
+		id_str:string,
+		name:string,
+		screen_name:string,
+		location:string,
+		url:string,
+		description:string,
+		translator_type:string,
+		protected:string,
+		verified:string,
+		followers_count:string,
+		friends_count:string,
+		listed_count:string,
+		favourites_count:string,
+		created_at:string,
+		utc_offset:string,
+		time_zone:string
+	>
+)
+
+ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
+STORED AS TEXTFILE
+LOCATION '/apps/hive/warehouse/raw_tweets/';
+
+
+
+
